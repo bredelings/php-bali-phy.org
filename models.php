5bbb7f20abfb031a91de1dac93ef80bf392d711c
Benjamin Redelings <benjamin.redelings@duke.edu>
2014-09-09 17:48:01 -0400

    Add page on models.
    Former-commit-id: f37750f751653ab3bb5578db88f0c17a09b77623

---
+++ models.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <?php $page='Models'; ?>
+    <?php require('lib/phplib.inc') ?>
+    <?php baliphy_head(); ?>
+    <title>BAli-Phy: Software for Bayesian Estimation of Alignment and Phylogeny</title>
+    <style type="text/css">
+ul.changelog li {font-weight: bold; color: blue}
+ul.changelog li li {font-weight: normal; color: black}
+.release_dates ul {display: inline; padding: 0; padding-left: 0.5}
+.release_dates li {display: inline; padding-left: 1em; white-space:nowrap}
+    </style>
+  </head>
+  <body>
+    <?php baliphy_header(); ?>
+
+    <table class="layout">
+      <tr>
+	<?php dirtree(); ?>
+	<td class="center" valign="top">
+	  <!-- ?php navbar(); ? -->
+	  <div class="main">
+	  <h2>Models</h2>
+
+	  <p>BAli-Phy has been extended to handed generic models.  This ability is under rapid development, and documentation is not yet available.</p>
+
+	  <p>The modelling language is based on Haskell.  Some example files are <a href="https://github.com/bredelings/BAli-Phy/blob/master/examples/Models">here</a>.</p>
+
+	  <p>For example, you might run <b>bali-phy -m CoalMine.hs --iter=1000</b> to perform a poisson regression.</p>
+
+	  
+	</div>
+	  </td>	      
+      </tr>
+    </table>
+    
+    <?php baliphy_footer(); ?>
+  </body>
+</html>
