9bd1fa834172cde49d11c039be3e4e8fbc5fb3fa
Benjamin Redelings <benjamin.redelings@duke.edu>
2014-09-16 17:32:22 -0400

    Make sure 'Graphical Models' gets highlighted, and also doesn't wrap.
    Former-commit-id: f4ba340014c1aaf55747ad5f8eab111dfb1360e4

--- models.php
+++ models.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
   <head>
-    <?php $page='Models'; ?>
+    <?php $page='Graphical&nbsp;Models'; ?>
     <?php require('lib/phplib.inc') ?>
     <?php baliphy_head(); ?>
     <title>BAli-Phy: Software for Bayesian Estimation of Alignment and Phylogeny</title>
