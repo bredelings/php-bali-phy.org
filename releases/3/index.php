83740f3d64cd9b38371904498742119998418991
Benjamin Redelings <benjamin.redelings@gmail.com>
2016-10-19 11:14:45 -0400

    Add page for version 3 changes.
    Former-commit-id: c9b36fa03ecafa62954072121c5269814d0ba8c3

--- releases/3/index.php
+++ releases/3/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
   <?php $top="../.."; ?>
-  <?php $page="Release 2.3 *"; ?>
+  <?php $page="Unreleased 3 (alpha)"; ?>
   <?php require("{$top}/lib/phplib.inc") ?>
   <head>
     <?php baliphy_head(); ?>
@@ -18,7 +18,7 @@
 	  <div class="main">
 	  <h2><a name="news">Release Notes</a></h2>
 
-	  <div class="release_dates"><b style="font-size:125%">Version 3:</b>
+	  <div class="release_dates"><b style="font-size:125%">Version 3 (unreleased, in development):</b>
           <ul>
 	    <li><b>[3.0]</b> - ??-??-2017</li>
 	  </ul>
@@ -27,17 +27,18 @@
 	  <ul class="changelog">
 	    <li>Major new features
 	      <ul>
-		<li><b>[planned]</b> ??</li>
+		<li></li>
 	      </ul>
 	    </li>
 
 	    <li>Speed
-	      <ul>
-		<li><span style="color:red">[??% faster]</span> Cache tree prior.</li>
+	    <ul>
+		<li><span style="color:red">[??% faster]</span></li>
+		<!-- li><span style="color:red">[??% faster]</span> Cache tree prior.</li>
 		<li><span style="color:red">[??% faster]</span> Avoid recomputing alignment matrix.</li>
 		<li><span style="color:red">[??% faster]</span> Eliminate alignment index matrices: Faster computation.</li>
 		<li><span style="color:red">[??% faster]</span> Faster computation when lots of gaps (fixed A).</li>
-		<li><span style="color:red">[??% faster]</span> Faster computation when lots of gaps (variable A).</li>
+		<li><span style="color:red">[??% faster]</span> Faster computation when lots of gaps (variable A).</li -->
 	      </ul>
 	    </li>
 
@@ -46,20 +47,25 @@
 		<li><b>04/14/16</b> Allow specifying constant model parameters.</li>
 		<li><b>04/14/16</b> Add Jukes-Cantor model name.</li>
 		<li><b>06/21/16</b> Make priors stop favoring large trees when lots of leaves</li>
-		<li><b>[planned]</b> Allow specifying random parameters.</li>
+		<!-- li><b>[planned]</b> Allow specifying random parameters.</li -->
 	      </ul>
 	    </li>
 
 
 	    <li>MCMC
-	      <ul>
+	    <ul>
+	      <li></li>
+	      <!--
 		<li><b>[planned]</b> Improve burnin by resampling pairwise A + branch length.
 		<li><b>[planned]</b> Improve mixing by resampling all pairwise As + (lambda,epsilon).
+		-->
 	      </ul>
 	      </li>
 
 	    <li>Internals
-	      <ul>
+	    <ul>
+	      <li></li>
+	      <!-- 
 		<li><b>10/22/15</b> Allow sharing unchanged steps when called result changes.</li>
 		<li><b>03/17/16</b> Move tree into the machine.</li>
 		<li><b>04/07/16</b> Compute likelihoods in separate contexts - no more invalid indices/CLs</li>
@@ -69,6 +75,7 @@
 		<li><b>06/09/16</b> Incremental GC (part 1) - unmap unused steps/results <em>before</em> we destroy regs. </li>
 		<li><b>06/22/16</b> Cache tree prior</b>
 		<li><b>[planned]</b> Compute likelihood in the machine.</li>
+	      -->
 	      </ul>
 	    </li>
 
@@ -79,26 +86,28 @@
 	    </li>
 
 	    <li>Input
-	      <ul>
-		<li><b>??</b> ??</li>
+	    <ul>
+	      <li></li>
+		<!--- li><b>??</b> ??</li -->
 	      </ul>
 	    </li>
 
 	    <li>Summarization tools
-	      <ul>
-		<li><em>bp-analyze.pl</em> - ??.</li>
+	    <ul>
+	      <li></li>
+	      <!-- <li><em>bp-analyze.pl</em> - ??.</li> -->
 	      </ul>
 	    </li>
 
 	    <li>Changes
-	      <ul>
-		<li><b>04/27/16</b> - Clean up command-line options somewhat.</li>
-	      </ul>
+	    <ul>
+	      <li><b>04/27/16</b> - Clean up command-line options somewhat.</li>
+	    </ul>
 	    </li>
 	      
 	    <li>Fixes
 	      <ul>
-		<li>4/14/16</li> Fix gwF to actually use 'f' parameter.
+		<li><b>4/14/16</b> Fix gwF to actually use 'f' parameter.</li>
 	      </ul>
 	    </li>
 	  </ul>
--- releases/index.php
+++ releases/index.php
@@ -1,8 +1,11 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
   <?php $top='..'; ?>
-  <?php $page='Releases'; ?>
+  <?php $page='Versions'; ?>
   <?php require("{$top}/lib/phplib.inc") ?>
+  <style>
+.main > ul li {padding: 0.5em}    
+  </style>
   <head>
     <?php baliphy_head(); ?>
     <title>BAli-Phy: Software for Bayesian Estimation of Alignment and Phylogeny</title>
@@ -19,7 +22,8 @@
 	  <h2><a name="news">Release Notes</a></h2>
 
           <ul>
-	    <li><a href="./2.3/">BAli-Phy 2.3</a>
+	    <li>(<a href="./3/"><em>BAli-Phy 3 alpha</em></a>: <em><b>unreleased/development</b></em> -- <a href="https://github.com/bredelings/BAli-Phy">github.com:bredelings/BAli-Phy</a>)
+	    <li><a href="./2.3/">BAli-Phy 2.3</a>: <b>current stable release</b> -- [<a href="../download.php">Download</a>]
 	    <li><a href="./2.2/">BAli-Phy 2.2</a>
 	    <li><a href="./2.1/">BAli-Phy 2.1</a>
 	    <li><a href="./2.0/">BAli-Phy 2.0</a>
