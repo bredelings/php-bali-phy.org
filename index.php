07c6123ea6c4e7f11918e9c846598f96b298672f
Benjamin Redelings <benjamin.redelings@gmail.com>
2017-10-23 17:46:21 -0400

    Update front page.
    Former-commit-id: 733258280682b09d10a40c7c73980d6f7c0044ed

--- index.php
+++ index.php
@@ -28,8 +28,8 @@
 	    <h3><a name="intro">Introduction</a></h3>
 
 	    <div style="margin-left:1em">
-	      <p>08/14/17: <strong>BAli-Phy 3.0-beta3 released</strong> - <a href="download.php">Download</a><br/>
-		Speed and memory improvements (<a href="releases/3/">more news</a>)</p>
+	      <p>10/23/17: <strong>BAli-Phy 3.0-beta4 released</strong> - <a href="download.php">Download</a><br/>
+		Ease-of-use and speed improvements (<a href="releases/3/">more news</a>)</p>
 
 	      </div>
 
