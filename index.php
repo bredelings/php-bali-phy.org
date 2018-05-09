f209dd27f74d3bc77d522bb65b56c06c4487a05e
Benjamin Redelings <benjamin.redelings@gmail.com>
2018-05-09 17:24:06 +0300

    Update to 3.1.4.
    Former-commit-id: 5d340eef83e4489455ee6a02afce8d89ff598ece

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
 	    <h3><a name="intro">Introduction</a></h3>
 
 	    <div style="margin-left:1em">
-	      <p>05/02/18: <strong>BAli-Phy 3.1 released</strong> - <a href="download.php">Download</a><br/>
+	      <p>05/09/18: <strong>BAli-Phy 3.1.4 released</strong> - <a href="download.php">Download</a><br/>
 		Show priors clearly (<a href="releases/3/">release notes</a>)</p>
 
 	      </div>
