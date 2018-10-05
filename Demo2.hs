942705a67edfc403dedcf38df90974ae9847058c
Benjamin Redelings <benjamin.redelings@gmail.com>
2018-10-05 17:15:40 -0400

    Remove {;} from Demo*.hs files.
    Former-commit-id: eff0f8dd3b3e05804ce2e51bf9f521a4753a305d

--- Demo2.hs
+++ Demo2.hs
@@ -1,15 +1,16 @@
 module Demo2 where
-{
-import Distributions;
+
+import Distributions
 
 main = do
-{
-  xs <- iid 10 (normal 0.0 1.0);
-  Log "xs" xs;
 
-  let {ys = map (\x -> x*x) xs};
-  Log "ys" ys;
+  xs <- sample $ iid 10 (normal 0.0 1.0)
 
-  Log "sum" (sum ys);
-}
-}
+  let ys = map (\x -> x*x) xs
+
+  return (Nothing,[
+           ("xs",(Just xs,[])),
+           ("ys",(Just ys,[])),
+           ("sum",(Just $ sum ys,[]))
+           ])
+
