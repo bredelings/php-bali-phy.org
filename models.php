f9e7e90837dde1b0efca05abe460872f585ebe44
Benjamin Redelings <benjamin.redelings@gmail.com>
2018-11-16 13:01:36 -0500

    Update models page again.
    Former-commit-id: 5f66bacdb43ca88e26db804530b30d23ba72fa98

--- models.php
+++ models.php
@@ -7,11 +7,19 @@
     <?php baliphy_head(); ?>
     <title>BAli-Phy: Software for Bayesian Estimation of Alignment and Phylogeny</title>
     <link rel="stylesheet" href="highlight/styles/arta2.css"></link>
+    <style type='text/css'>
+code {background: #f0f0f0}      
+    </style>
   </head>
   <body>
     <?php baliphy_header(); ?>
 
     <script src="highlight/highlight.pack.js"></script>
+    <script>
+      $('code').each(function(i, block) {
+          hljs.highlightBlock(block);
+      });
+    </script>
     <script>hljs.initHighlightingOnLoad();</script>
     <table class="layout">
       <tr>
@@ -23,58 +31,67 @@
 
 	  <h2>Rapid, easy model development</h2>
 
-	  <p>BAli-Phy contains a simple language for expressing probabilistic models.  The goal of this language is to allow researchers to spend there time designing models, not MCMC software.  The inference should take care of itself.</p>
-	  <p>BAli-Phy's modelling language uses Haskell syntax:
+	  <p>BAli-Phy contains a simple language for expressing probabilistic models as programs.  Such languages are called <em>probabilistic programming languages</em>. Other well-known languages include <a href="https://www.mrc-bsu.cam.ac.uk/software/bugs/">BUGS</a>, <a href="https://revbayes.github.io">RevBayes</a>, and <a href="http://mc-stan.org">Stan</a>.</p>
 
-<pre><code class="haskell"><?php include('Demo1.hs') ?></code></pre>
+	  <p>The goal of this language is to allow researchers to spend their time designing models instead of MCMC software.  The inference should take care of itself.</p>
 
-	  <p>Example files are <a href="https://github.com/bredelings/BAli-Phy/blob/master/examples/models">here</a>.  For example, you might run <b>bali-phy -m CoalMine.hs --iter=1000</b> to perform a poisson regression.</p>
+	  <p>The modeling language uses Haskell syntax.  For example, here is a short program that performs linear regression:</p>
 
-	  <p>Languages for expressing probabilistic models are called <em>probabilistic programming languages</em>.  Other well-known languages include BUGS, RevBayes, and Stan.</p>
+<pre><code class="haskell"><?php include('LinearRegression.hs') ?></code></pre>
 
-
-<h2>Dynamic graphical models</h2>
-<p>BAli-Phy attempts to extend this paradigm by implementing <em>dynamic</em> graphical models via <a href="http://en.wikipedia.org/wiki/Probabilistic_programming_language">probabilistic programming</a>.  The approach is similar to <a href="http://projects.csail.mit.edu/church/wiki/Church">Church</a> and <a href="http://probcomp.csail.mit.edu/venture/">Venture</a> because it uses execution traces to represent the dynamic graph and is <a href="http://en.wikipedia.org/wiki/Turing_completeness">Turing-complete</a>.  The approach
 <ul>
-<li> allows a single model to express a <strong>changing graph</strong>.
-<li> treats <strong>functions</strong> as first-class objects and allows <strong>recursive</strong> functions.
-<li> allows the use of <strong>data-structures</strong> with random fields.
-<li> will allow random numbers of random variables.
+  <li><code class="haskell">let f x = b*x +a</code> defines the prediction function f</li>
+  <li><code class="haskell">b &lt;- sample $ normal 0.0 1.0</code> places a prior on the slope of the line.</li>
+  <li><code class="haskell">observe y (normal mu_y s)</code> says that the data y comes from a normal distribution with mean mu_y and standard deviation s.</li>
 </ul>
-The modelling framework is under rapid development, and I haven't written much documentation yet.</p>
 
-<h2>Random data structures</h2>
-<p>We can also sample random data structures.  For example, the <em>iid</em> distribution returns a random list.  We can apply the <em>map</em> and <em>sum</em> operations to such lists to sample a sum of squares.
+	  <p>You can find this file <a href="https://github.com/bredelings/BAli-Phy/blob/master/examples/models">here</a> and run it as <b>bali-phy -m LinearRegression.hs --iter=1000</b>.</p>
+
+	  <p>This language is beta quality.  There is currently no type-checking.  The language should be ready sometime during 2019.</p>
+
+	  <h2>Dynamic graphical models</h2>
+	  <p>BAli-Phy attempts to extend the graphical paradigm by allowing the graph to change and treating functions as first-class objects.  This enables</p>
+	  
+	    <ol>
+	      <li> random control flow (if-then-else, loops) </li>
+	      <li> random data structures</li>
+	      <li> random numbers of random variables (coming soon)</li>
+	      <li> lazy evaluation of random variables (coming soon)</li>
+	    </ol>
+
+<h3>Random data structures</h3>
+<p>The <em>iid</em> distribution returns a list of random values from another distribution.  We can apply the <em>map</em> and <em>sum</em> operations to such lists to sample a sum of squares.</p>
 <pre><code class="haskell"><?php include('Demo2.hs') ?></code></pre>
-Here <em>(\x -> x*x)</em> describes an un-named function that takes an argument <em>x</em> and returns <em>x*x</em>.
+<p>Here <em>(\x -> x*x)</em> describes an un-named function that takes an argument <em>x</em> and returns <em>x*x</em>.
 </p>
 
 
-<h2>Dynamic graphs I: if-then-else</h2>
-<p>The graph in a traditional graphical model never changes.  Changing the graph involves changing the model.  In contrast, <em>dynamic</em> graphical models use a single model to specify a changing graph.  One thing that leads to a changing graph is control-flow statements like <em>if-then-else</em>:
+<h3>Changing graphs I: if-then-else</h3>
+<p>The modeling language can handle graphs that change. One thing that leads to a changing graph is control-flow statements like <em>if-then-else</em>:</p>
 <pre><code class="haskell"><?php include('Demo3.hs') ?></code></pre>
 While <em>x</em> always depends on <em>i</em>, it depends on either <em>y</em> or <em>z</em>, but not both.  The dynamic graphical model can represent this by updating the graph to handle the cases where <em>i=0</em> or <em>i=1</em>:
-<div align="center">
-<img src="graphical_model3.svg" style="padding:1em; padding-left: 3em; padding-right:3em"/>
-<img src="graphical_model4.svg" style="padding:1em; padding-left: 3em; padding-right:3em"/>
+
+<div>
+<img src="graphical_model3.svg" style="width:10em; padding:1em; padding-left: 3em; padding-right:3em"/>
+<img src="graphical_model4.svg" style="width:10em; padding:1em; padding-left: 3em; padding-right:3em"/>
 </div>
 In contrast, a traditional graphical model makes <em>x</em> always depend on everything that it <em>might</em> depend on.
-<div align="center">
-<img src="graphical_model5.svg" style="padding:1em; padding-left: 3em; padding-right:3em"/>
+<div>
+<img src="graphical_model5.svg" style="width:10em; padding:1em; padding-left: 3em; padding-right:3em"/>
 </div>
 
-<h2>Dynamic graphs II: arrays with random subscripts</h2>
-<p>Traditional graphical modelling languages, like BUGS, allow arrays of random variables.  However, they do not allow selecting a random element of these arrays.  Dynamic graphs allow random subscripts. This can be used to divide observations into categories.  Here different elements of <em>ys</em> will be exactly equal, if they belong to the same category:
+<h3>Changing graphs II: arrays with random subscripts</h3>
+<p>Traditional graphical modelling languages, like BUGS, allow arrays of random variables.  However, they do not allow selecting a random element of these arrays.  Dynamic graphs allow random subscripts. This can be used to divide observations into categories.  Here different elements of <em>ys</em> will be exactly equal, if they belong to the same category:</p>
 
 <pre><code class="haskell"><?php include('Demo4.hs') ?></code></pre>
-  In Haskell, we use the <em>!!</em> operator to subscript a list.  The C equivalent of <em>xs!!(categories!!i))</em> would be something like <em>xs[categories[i]]</em>.
+<p>  Haskell uses the <em>!!</em> operator to subscript a list.  The C equivalent of <em>xs!!(categories!!i))</em> would be something like <em>xs[categories[i]]</em>.</p>
 
-<h2>Random sampling via recursive functions: Brownian Bridge</h2>
-<p>We can use recursive functions to randomly sample lists of random values.  Here, we define a function <em>random_walk</em> that produces a list of random values starting from <em>x0</em>.
+<h3>Random sampling via recursive functions: Brownian Bridge</h3>
+<p>We can use recursive functions to randomly sample lists of random values.  Here, we define a function <em>random_walk</em> that produces a list of random values starting from <em>x0</em>.</p>
 <pre><code class="haskell"><?php include('Demo5.hs') ?></code></pre>
   The argument <em>f</em> is a function.  In Haskell, we write <em>f x</em> instead of <em>f(x)</em> to apply a function.  Here, <em>f x</em> gives the distribution of the point after <em>x</em>.</p>
 
-<p>The <em>Observe</em> command specifies observed data.  Here we observe that the next point after element 10 of <em>zs</em> is 2.0.  This constrains the random walk to end at 2.0, creating a Brownian Bridge.</p>
+<p>The <code class="haskell">observe</code> command specifies observed data.  Here we observe that the next point after element 10 of <em>zs</em> is 2.0.  This constrains the random walk to end at 2.0, creating a Brownian Bridge.</p>
 	</div>
 	  </td>	      
       </tr>
