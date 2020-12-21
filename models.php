66cb79765dfd613eec14984451f8dbb6f5b170da
Benjamin Redelings <benjamin.redelings@gmail.com>
2020-12-21 08:10:49 -0800

    Mention PyMC3

--- models.php
+++ models.php
@@ -32,7 +32,8 @@ code {background: #f0f0f0}
          <img src="developer/architecture.png" style="float:right;width:15em"/>
 	  <h2>Rapid, easy model development</h2>
 
-	  <p>BAli-Phy contains a simple language for expressing probabilistic models as programs.  Inference on parameters can then be performed automatically. Such languages are called <a href="https://en.wikipedia.org/wiki/Probabilistic_programming_language">probabilistic programming languages</a> (PPL). Other well-known PPLs include <a href="https://www.mrc-bsu.cam.ac.uk/software/bugs/">BUGS</a>, <a href="https://revbayes.github.io">RevBayes</a>, and <a href="http://mc-stan.org">Stan</a>. The goal of the language is to allow researchers to spend their time designing models instead of designing new inference programs.  The inference should take care of itself after the model is specified.</p>
+	  <p>BAli-Phy contains a simple language for expressing probabilistic models as programs.  Inference on parameters can then be performed automatically. Such languages are called <a href="https://en.wikipedia.org/wiki/Probabilistic_programming_language">probabilistic programming languages</a> (PPL). Other well-known PPLs include <a href="http://mc-stan.org">Stan</a>, <a href="https://docs.pymc.io/">PyMC3</a>, <a href="https://www.mrc-bsu.cam.ac.uk/software/bugs/">BUGS</a>, and <a href="https://revbayes.github.io">RevBayes</a>.
+            The goal of the language is to allow researchers to spend their time designing models instead of designing new inference programs.  The inference should take care of itself after the model is specified.</p>
 
 	  <h4>Language properties</h4>
 	  <p>The modeling language is a <a href="https://en.wikipedia.org/wiki/Functional_programming">functional language</a>, and uses <a href="https://www.haskell.org">Haskell</a> syntax.  Features currently implemented include:</p>
