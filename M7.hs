a4a4753c0f29a4407356cbd9b9b7d648dcf6bce3
Benjamin Redelings <benjamin.redelings@gmail.com>
2023-04-20 23:10:16 -0400

    Update M7.hs

--- M7.hs
+++ M7.hs
@@ -12,17 +12,17 @@ gtr_m7_model codons = do
 
     -- GTR model parameters
     let nucs = getNucleotides codons
-    sym <- symmetric_dirichlet_on (letter_pair_names nucs) 1
-    pi  <- symmetric_dirichlet_on (letters nucs) 1
+    sym <- prior $ symmetric_dirichlet_on (letter_pair_names nucs) 1
+    pi  <- prior $ symmetric_dirichlet_on (letters nucs) 1
     let gtr_model = gtr' sym pi nucs
 
     -- Positive selection model based on GTR
-    let pos_sel_model w = gtr_model & x3 codons & dNdS w
+    let pos_sel_model w = gtr_model +> x3 codons +> dNdS w
 
     -- M7 model parameters
-    mu    <- uniform 0 1            -- mean of dN/dS
-    gamma <- beta 1 10              -- spread of dN/dS
-    let m7_model = pos_sel_model & m7 mu gamma 4
+    mu    <- prior $ uniform 0 1            -- mean of dN/dS
+    gamma <- prior $ beta 1 10              -- spread of dN/dS
+    let m7_model = pos_sel_model +> m7 mu gamma 4
 
     let loggers =
             [ "gtr:sym" %=% sym
@@ -42,23 +42,23 @@ model seq_data = do
         tip_seq_lengths = get_sequence_lengths the_codons seq_data
 
     -- Tree
-    scale1 <- gamma 0.5 2
-    tree   <- uniform_labelled_tree taxa branch_length_dist
+    scale1 <- prior $ gamma 0.5 2
+    tree   <- prior $ uniform_labelled_tree taxa branch_length_dist
     let tree1 = scale_branch_lengths scale1 tree
 
     -- Indel model
-    logLambda   <- log_laplace (-4) 0.707
-    mean_length <- (1 +) <$> exponential 10
+    logLambda   <- prior $ log_laplace (-4) 0.707
+    mean_length <- (1 +) <$> prior (exponential 10)
     let imodel = rs07 logLambda mean_length tree
 
     -- Substitution model
     (m7_model, log_m7_smodel) <- gtr_m7_model the_codons
 
     -- Alignment
-    alignment <- random_alignment tree1 imodel tip_seq_lengths
+    alignment <- prior $ random_alignment tree1 imodel tip_seq_lengths
 
     -- Observation
-    seq_data ~> ctmc_on_tree tree1 alignment m7_model
+    observe seq_data $ ctmc_on_tree tree1 alignment m7_model
 
     return
         [ "tree1" %=% write_newick (make_rooted tree1)
