6b9b9debef8ae399361bf71303511a789fd08116
Benjamin Redelings <benjamin.redelings@gmail.com>
2023-11-27 12:28:00 -0500

    Update example code for phyloCTMC / phyloAlignment

--- InferTreeAlignment.hs
+++ InferTreeAlignment.hs
@@ -3,23 +3,22 @@ module Model where
 import           Probability
 import           Bio.Alignment
 import           Bio.Alphabet
+import           Bio.Sequence
 import           Tree
 import           Tree.Newick
 import           SModel
 import           IModel
-import           Probability.Distribution.OnTree
 import           System.Environment  -- for getArgs
 
 branch_length_dist topology branch = gamma (1/2) (2/fromIntegral n) where n = numBranches topology
 
 model seq_data = do
-    let taxa            = map sequenceName seq_data
-        tip_seq_lengths = get_sequence_lengths dna seq_data
+    let taxa            = getTaxa seq_data
+        tip_seq_lengths = get_sequence_lengths seq_data
 
     -- Tree
-    scale1 <- prior $ gamma (1/2) 2
-    tree   <- prior $ uniform_labelled_tree taxa branch_length_dist
-    let tree1 = scale_branch_lengths scale1 tree
+    scale <- prior $ gamma (1/2) 2
+    tree  <- prior $ uniform_labelled_tree taxa branch_length_dist
 
     -- Indel model
     indel_rate   <- prior $ log_laplace (-4) 0.707
@@ -33,27 +32,27 @@ model seq_data = do
     let tn93_model = tn93' dna kappa1 kappa2 freqs
 
     -- Alignment
-    alignment <- prior $ random_alignment tree1 imodel tip_seq_lengths
+    alignment <- prior $ phyloAlignment tree imodel scale tip_seq_lengths
 
     -- Observation
-    observe seq_data $ ctmc_on_tree tree1 alignment tn93_model
+    observe seq_data $ phyloCTMC tree alignment tn93_model scale
 
     return
-        [ "tree1" %=% write_newick tree1
+        [ "tree" %=% write_newick tree
         , "log(indel_rate)" %=% log indel_rate
         , "mean_length" %=% mean_length
         , "kappa1" %=% kappa1
         , "kappa2" %=% kappa2
         , "frequencies" %=% freqs
-        , "scale1" %=% scale1
+        , "scale" %=% scale
         , "|T|" %=% tree_length tree
-        , "scale1*|T|" %=% tree_length tree1
-        , "|A|" %=% alignment_on_tree_length alignment
+        , "scale*|T|" %=% tree_length tree * scale
+        , "|A|" %=% alignmentLength alignment
         ]
 
 main = do
     [filename] <- getArgs
 
-    seq_data <- load_sequences filename
+    seq_data <- mkUnalignedCharacterData dna <$> load_sequences filename
 
     return $ model seq_data
