c79507accd688d0fab1f7e2d2c517d885b8197b5
Benjamin Redelings <benjamin.redelings@gmail.com>
2024-05-26 13:45:51 +0100

    Just say the models work with 4.0beta

--- models.php
+++ models.php
@@ -163,9 +163,9 @@ code {background: #f0f0f0}
             <li><a href="#recursive_sampling">Recursive sampling: a Brownian Bridge</a></li -->
           </ol>
 
-          <div class="warningbox"><b>Note</b>: These examples won't work with version 3.  They will be part of version 4, which is expected to come out in early-to-mid 2023.<br/><br/>To run these examples now you can
+          <div class="warningbox"><b>Note</b>: These examples don't work with version 3.  They work with the beta releases of version 4.<br/><br/>To run these examples now you can
             <ul>
-              <li>download the <a href="https://github.com/bredelings/BAli-Phy/releases/tag/4.0-beta13">4.0-beta13release</a>, or</li>
+              <li>download the <a href="https://github.com/bredelings/BAli-Phy/releases/tag/4.0-beta13">4.0-beta13</a> release, or</li>
               <li>compile from the <a href="https://github.com/bredelings/BAli-Phy/">source on github.</a></li>
             </ul>
           </div>
