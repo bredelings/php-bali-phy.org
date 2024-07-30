8cf87dfc1f600192639dd20187d44cdfdb0e5da7
Benjamin Redelings <benjamin.redelings@gmail.com>
2024-07-29 20:46:51 -0400

    Update notes for version 4.

--- releases/4/index.php
+++ releases/4/index.php
@@ -18,40 +18,40 @@
 	  <div class="main">
 	  <h2><a name="news">Version 4 (unreleased)</a></h2>
 
-          <p>Version 4 is under development and will contain a number of new features.  You can preview it by
-            <a href="https://github.com/bredelings/BAli-Phy/releases">downloading a pre-release</a> or compiling the git master branch at
-            <a href="https://github.com/bredelings/BAli-Phy">https://github.com/bredelings/BAli-Phy</a>.</p>
+          <p>Version 4 is under development.  It already contains a number of new features, and more are in progress.
+          See the <a href="https://github.com/bredelings/BAli-Phy/blob/master/NEWS">NEWS</a> for current progress.</p>
 
-            <h3>New features</h3>
+          <p>You can try out the development by <a href="https://github.com/bredelings/BAli-Phy/releases">downloading a pre-release</a>.
+          You can also compile the <a href="https://github.com/bredelings/BAli-Phy">source code from github</a>.</p>
+
+          <h3>New features</h3>
+          <ul>
+            <li><b><a href="../../models.php">Model language</a></b> - describe your own phylogenetic models.</li>
+            <li><b>more flexible models</b> - easily change dimensions without reversible jump. (<em>partially implemented</em>)</li>
+            <li><b>Non-reversible and non-equilibrium markov models</b>.
             <ul>
-              <li><b><a href="../../models.php">probabilistic programming language</a></b> - describe your own phylogenetic models.</li>
-              <li><b>more flexible models</b> - easily change dimensions without reversible jump. (<em>partially implemented</em>)</li>
-              <li><b>Non-reversible and non-equilibrium markov models</b>.
-                <ul>
-                  <li>Compositional heterogeneity model (in progress)</li>
-                  <li>Non-reversible rate matrix.</li>
-                </ul>
-              </li>
-              <!-- li><b>type checking</b> for the model language</li -->
-              <!-- li><b>improved MCMC</b> - slice-sampling by doubling.</li -->
-              <!-- li><b>decreased RAM usage</b></li -->
-              <!-- li><b>better command-line model language</b></li -->
+              <li>Compositional heterogeneity model (in progress)</li>
+              <li>Non-reversible rate matrix.</li>
             </ul>
-
-          <h3>In progress features</h3>
-            <ul>
-              <li><b>Faster alignment</b> for long sequences.</li>
-	      <li><b>Time Trees and the relaxed clock</b>. Rooted trees implemented as a data structure within the language. (<em>partially implemented</em>)</li>
-              <li><b>Custom MCMC moves</b>. The ability to add custom MCMC transition kernels will be added. (<em>partially implemented</em>)</li>
-              <li><b>RNA stem models</b> - model RNA stems and loops (fixed alignments)</li>
+            </li>
+            <!-- li><b>type checking</b> for the model language</li -->
+            <!-- li><b>improved MCMC</b> - slice-sampling by doubling.</li -->
+            <!-- li><b>decreased RAM usage</b></li -->
+            <!-- li><b>better command-line model language</b></li -->
           </ul>
 
-          <p>See the <a href="https://github.com/bredelings/BAli-Phy/blob/master/NEWS">NEWS</a> file for more details</p>
+          <h3>In progress features</h3>
+          <ul>
+            <li><b>Faster alignment</b> for long sequences.</li>
+	    <li><b>Time Trees and the relaxed clock</b>. Rooted trees implemented as a data structure within the language. (<em>partially implemented</em>)</li>
+            <li><b>Custom MCMC moves</b>. The ability to add custom MCMC transition kernels will be added. (<em>partially implemented</em>)</li>
+            <li><b>RNA stem models</b> - model RNA stems and loops (fixed alignments)</li>
+            <li>Site and letter properties: evolutionary rate, positive selection, etc.</li>
+          </ul>
 
-
-
+          
 	</div>
-	  </td>
+	</td>
       </tr>
     </table>
     
