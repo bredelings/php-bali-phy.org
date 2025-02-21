3f34b6a50f0619c96990359ded1111fb963aea71
Benjamin Redelings <benjamin.redelings@gmail.com>
2025-02-20 23:31:20 -0500

    Update the update-demos.sh script.

--- update-demos.sh
+++ update-demos.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+BALIPHYDIR=$HOME/Devel/bali-phy/jj/
+
 for i in {1..5} ; do
-    cp ~/Devel/bali-phy/git/tests/prob_prog/demos/${i}/Model.hs Demo${i}.hs
+    cp ${BALIPHYDIR}/tests/prob_prog/demos/${i}/Model.hs Demo${i}.hs
 done
 
-cp ~/Devel/bali-phy/git/tests/prob_prog/regression/LinearRegression.hs .
-cp ~/Devel/bali-phy/git/tests/prob_prog/sample_tree/3/sample.hs random_tree.hs
-cp ~/Devel/bali-phy/git/tests/prob_prog/infer_tree/1/Model.hs InferTreeAlignment.hs
+cp ${BALIPHYDIR}/tests/prob_prog/regression/LinearRegression.hs .
+cp ${BALIPHYDIR}/tests/prob_prog/sample_tree/3/sample.hs random_tree.hs
+cp ${BALIPHYDIR}/tests/prob_prog/infer_tree/1/Model.hs InferTreeAlignment.hs
+
 #brittany --write-mode=inplace InferTreeAlignment.hs --columns 80
 fourmolu --column-limit 120 -i M7.hs
